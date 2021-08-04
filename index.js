1a6dca897e026eff7a679439c50a420debffc36d
JaJamaRu <shimizu@ray-up.com>
2021-08-04 15:43:10 +0900

    [Change] index.html and index.js

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const btns = document.querySelectorAll('button');
+const comment = document.querySelector('.comment');
+const username = document.querySelector('.name');
+const jobTitle = document.querySelector('.job-title');
+const picture = document.querySelector('.picture');
+
+const testimonials = [
+  {
+    username: 'Tanya Sinclair',
+    jobTitle: 'UX Engineer',
+    comment:
+      '  “ I’ve been interested in coding for a while but never taken the jump, until now. I couldn’t recommend this course enough. I’m now in the job of my dreams and so excited about the future. ” ',
+    picture: './images/image-tanya.jpg',
+  },
+  {
+    username: 'John Tarkpor',
+    jobTitle: 'Junior Front-end Developer',
+    comment:
+      ' “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ” ',
+    picture: './images/image-john.jpg',
+  },
+];
 
 btns.forEach(function (btn) {
   btn.addEventListener('click', () => {
@@ -6,15 +27,15 @@ btns.forEach(function (btn) {
   });
 });
 
-const comment = document.querySelector('.comment');
-const name = document.querySelector('.name');
-const jobTitle = document.querySelector('.job-title');
-const picture = document.querySelector('.picture');
-
+let num = 1;
 function switchTestimonial() {
-  comment.innerText =
-    ' “ If you want to lay the best foundation possible I’d recommend taking this course. The depth the instructors go into is incredible. I now feel so confident about starting up as a professional developer. ” ';
-  name.innerText = 'John Tarkpor';
-  jobTitle.innerText = 'Junior Front-end Developer';
-  picture.src = './images/image-john.jpg';
+  if (num >= testimonials.length) {
+    num = 0;
+  }
+  comment.innerText = testimonials[num].comment;
+  username.innerText = testimonials[num].username;
+  jobTitle.innerText = testimonials[num].jobTitle;
+  picture.src = testimonials[num].picture;
+
+  num++;
 }
